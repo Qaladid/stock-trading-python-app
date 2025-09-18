4a28fda80d5921eaf7d74847d0094d29beb20750
Qaladid <abdiaziz11410@gmail.com>
2025-09-18 03:00:39 +0300

    added the necessary folders and files for the project

--- script.py
+++ script.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+LIMIT = 100
 
-url = f'https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit=1000&sort=ticker&apiKey={POLYGON_API_KEY}'
+url = f'https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}'
 
 response = requests.get(url)
 tickers = []
